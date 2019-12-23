810d55926e10f5f3ddfa8f70eadadd782346c069
xx2t <zekuna.fitusa@gmail.com>
2019-12-23 15:10:03 -0600

    Cities!

---
+++ Assets/Scripts/LocationAngle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class LocationAngle : MonoBehaviour
+{
+    Transform planet;
+    public string cityName;
+    TextMeshPro displayText;
+    public int load;
+
+    void Start(){
+        planet=GameObject.Find("Planet").transform;
+        displayText=this.transform.GetChild(0).GetComponent<TextMeshPro>();
+        this.transform.LookAt(planet);
+        this.GetComponent<MeshRenderer>().enabled=false;
+        displayText.text=cityName;
+        displayText.gameObject.SetActive(false);
+    }
+    void OnMouseOver(){
+        displayText.gameObject.SetActive(true);
+        if(Input.GetMouseButtonDown(0)){
+            SceneManager.LoadScene(load);
+        }
+    }
+    void OnMouseExit(){
+        displayText.gameObject.SetActive(false);
+    }
+}
