11b11056da02df37ae09f7332cdaff1441a24ea0
xx2t <zekuna.fitusa@gmail.com>
2020-01-03 10:44:27 -0600

    Implementing Destruction

---
+++ Assets/Scripts/buildingGuides.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingGuides : MonoBehaviour
+{
+    Outline outline;
+    structureManagement sm;
+    public int cost;
+    public bool selected;
+    bool over;
+    void Start(){
+        sm=GameObject.Find("Management").GetComponent<structureManagement>();
+        outline=this.GetComponent<Outline>();
+        outline.enabled=false;
+    }
+    void FixedUpdate(){
+        if(sm.targets.Contains(this.gameObject)){
+            selected=true;
+        }else if(over){
+            selected=true;
+        }else{
+            selected=false;
+        }
+        if(!selected){
+            outline.enabled=false;
+        }else{
+            outline.enabled=true;
+        }
+    }
+    void OnMouseOver(){
+        over=true;
+        selected=true;
+        outline.OutlineColor=Color.red;
+        if(Input.GetMouseButtonDown(0)&&Input.GetKey(KeyCode.LeftControl)){
+            sm.LinkToTargets(this.gameObject);
+        }else if(Input.GetMouseButtonDown(0)){
+            sm.ClearTargets();
+            sm.LinkToTargets(this.gameObject);
+        }
+        if(Input.GetMouseButtonDown(1)&&Input.GetKey(KeyCode.LeftControl)){
+            sm.RemoveFromTargets(this.gameObject);
+        }
+    }
+    void OnMouseExit(){
+        over=false;
+        if(!sm.targets.Contains(this.gameObject)){
+            selected=false;
+        }
+    }
+}
---
+++ Assets/Scripts/structureManagement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class structureManagement : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public int cost;
+    public void LinkToTargets(GameObject selected){
+        if(targets.Count==0){
+            cost=0;
+            targets.Add(selected);
+            Debug.Log(selected.name +"added");
+            cost+=selected.GetComponent<buildingGuides>().cost;
+        }else{
+            targets.Add(selected);
+            Debug.Log(selected.name +"added");
+            cost+=selected.GetComponent<buildingGuides>().cost;
+        }
+    }
+    void Update(){
+        if(Input.GetMouseButtonDown(1)&&!Input.GetKey(KeyCode.LeftControl)){
+            ClearTargets();
+        }
+    }
+    public void RemoveFromTargets(GameObject selected){
+        if(targets.Contains(selected)){
+            targets.Remove(selected);
+        }
+    }
+    public void ClearTargets(){
+        foreach(GameObject selected in targets){
+            targets.Remove(selected);
+        }
+    }
+}
